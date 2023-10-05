1500d40d4bc9cbf884fb5802c22ed0ee7427234e
sodiee <mathi.carlsen@gmail.com>
2023-10-05 11:57:29 +0200

    Initial commit

---
+++ Skeleton/src/Server.java
@@ -0,0 +1,17 @@
+package Skeleton.src;
+
+import java.net.*;
+public class Server {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)throws Exception {
+		ServerSocket welcomeSocket = new ServerSocket(6789);
+		while (true) {
+			Socket connectionSocket = welcomeSocket.accept();
+			(new ServerThread(connectionSocket)).start();
+		}
+	}
+
+}
